69990eab2e2df1dab348a562444f48c83e1c6023
maabbas <mx38x@icloud.com>
2023-07-01 18:31:37 +0300

    hi

--- go-reloaded/main.go
+++ go-reloaded/main.go
@@ -12,14 +12,14 @@ func main() {
 	if osNo != 3 {
 		return
 	} else {
-		input := os.Args(1)
-		output := os.Args(2)
+		input := os.Args[1]
+		output := os.Args[2]
 		file, err := os.Create(output)
 		if err != nil {
 			fmt.Println(err.Error())
 			return
 		}
-		defer file.close()
+		defer file.Close()
 		text := readFile(input)
 		text = correct(text)
 
@@ -35,14 +35,12 @@ func readFile(name string) string {
 	file, err := os.Open(name)
 	if err != nil {
 		printError(err)
-		continue
 	}
 	f, err := ioutil.ReadAll(file)
 	if err != nil {
 		printError(err)
-		continue
 	}
-	text = f
+	text = string(f)
 	file.Close()
 	return text
 }
@@ -52,12 +50,12 @@ func hex(text string) string {
 	for _, c := range text {
 		power := 0
 		if c >= '0' && c <= '9' {
-			x := piscine.Atoi(string(c))
-			z += c * IterativePower(16, power)
+			x := Atoi(string(c))
+			z += x * IterativePower(16, power)
 			power++
 		} else {
-			x := piscine.Atoi(string((c - 'A')))
-			z += c * IterativePower(16, power)
+			x := Atoi(string((c - 'A')))
+			z += x * IterativePower(16, power)
 			power++
 		}
 	}
@@ -87,4 +85,49 @@ func an(text string) string {
 
 func punctuations(text string) string {
 
+}
+
+
+
+func printError(err error) {
+	fmt.Println("ERROR: " + err.Error())
+	os.Exit(1)
+}
+
+func IterativePower(nb int, power int) int {
+	nn := 1
+	if power < 0 {
+		return 0
+	} else if power == 0 {
+		return 1
+	} else if power > 0 {
+		for i := 1; i <= power; i++ {
+			nn *= nb
+		}
+	}
+	return nn
+}
+
+
+func Atoi(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+	sign := 1
+	result := 0
+	if s[0] == '+' || s[0] == '-' {
+		if s[0] == '-' {
+			sign = -1
+		}
+		s = s[1:]
+	}
+	for _, ch := range s {
+		if ch < '0' || ch > '9' {
+			return 0
+		}
+		digit := int(ch - '0')
+		result = result*10 + digit
+	}
+	result *= sign
+	return result
 }
